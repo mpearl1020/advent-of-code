eb85a38f0eb3153ff0c7f62c04988d7d6cd8d80b
Matthew Pearl <mpearl@Matthews-MBP-3.fios-router.home>
2023-06-16 06:24:15 -0400

    debugging node children

--- 2022/day7/day7.c
+++ 2022/day7/day7.c
@@ -6,6 +6,7 @@
 #define MAX_NAME_LENGTH 100
 
 typedef struct treeNodeTag {
+    char *name; // FOR DEBUGGING
     long size;
     int childIdx;
     struct treeNodeTag *parent;
@@ -27,8 +28,10 @@ int main() {
     fileTree.root = NULL;
 
     treeNode *currNode = (treeNode *) malloc(sizeof(treeNode));
+    currNode->name = (char *) malloc(MAX_NAME_LENGTH * sizeof(char));
+    currNode->name = "/\0"; // FOR DEBUGGING
     currNode->size = 0;
-    currNode->childIdx = 0;
+    currNode->childIdx = -1;
     currNode->parent = NULL;
     currNode->children = NULL;
 
@@ -44,6 +47,11 @@ int main() {
     <size> -> create node for file
     */
     while ((read = getline(&line, &len, fin)) != -1) {
+        printf("LINE: %s", line);
+        printf("CURR NODE NAME: %s\n", currNode->name);
+        // for (int i = 0; i < numSubs; i++) {
+        //     printf("CHILD NAME: %s\n", currNode->children[i]->name);
+        // }
         char command[5];
         strlcpy(command, &line[0], 5);
         if (line[0] == '$') {
@@ -52,31 +60,37 @@ int main() {
                     currNode = currNode->parent;
                 } else { // cd <dir>
                     if (line[5] != '/') {
-                        numSubdirs = 0;
+                        numSubs = 0;
+                        currNode->childIdx++;
+                        currNode = currNode->children[currNode->childIdx];
                     }
                 }
             }
         } else { // directory or file encountered
             numSubs++;
             treeNode *newNode = (treeNode *) malloc(sizeof(treeNode));
+            newNode->name = (char *) malloc(MAX_NAME_LENGTH * sizeof(char));
             newNode->size = 0;
-            newNode->childIdx = 0;
+            newNode->childIdx = -1;
             newNode->parent = currNode;
             if (numSubs == 1) {
                 currNode->children = (treeNode **) malloc(sizeof(treeNode *));
             } else {
-                treeNode **newChildrenArr = (treeNode **) realloc(currNode->children, numSubs * sizeof(treeNode *));
+                currNode->children = (treeNode **) realloc(currNode->children, numSubs * sizeof(treeNode *));
             }
             if (strcmp(command, "dir \0") == 0) { // add each dir node to current node's children array
-                currNode->children[numSubdirs - 1] = newNode;
+                strlcpy(newNode->name, &line[4], strlen(line) - 4);
+                currNode->children[numSubs - 1] = newNode;
             } else { // add each file to current node's children
                 long size = strtol(line, &line, 10);
                 newNode->size = size;
-                currNode->children[numSubdirs - 1] = newNode;
+                currNode->children[numSubs - 1] = newNode;
             }
+            free(newNode);
         }
     }
 
+    free(currNode);
     free(line);
     fclose(fin);
 
